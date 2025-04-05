11f17ef91c4fe325f442d8d596316f4d0ce5ea0f
phongvudzz <phongvulearning@gmail.com>
2025-04-05 12:56:15 +0700

    fix build

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -4,6 +4,7 @@ export default async function Home() {
   const posts = await prisma.post.findMany({
     include: { author: true },
   });
+
   return (
     <h1 className="text-3xl font-bold underline">
       {JSON.stringify(posts, null, 2)}
