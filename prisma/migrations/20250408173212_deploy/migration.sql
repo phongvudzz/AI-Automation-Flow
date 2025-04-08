f1b4780facab4830e98e9bc3b6b1f5c3efdc2523
phongvudzz <phongvulearning@gmail.com>
2025-04-09 00:35:30 +0700

    Created wofklow and shown list workflow on UI

--- prisma/migrations/20250408173212_deploy/migration.sql
+++ prisma/migrations/20250408173212_deploy/migration.sql
@@ -3,7 +3,7 @@ CREATE TABLE "Workflow" (
     "id" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
     "name" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
+    "description" TEXT,
     "definition" TEXT NOT NULL DEFAULT '{}',
     "executionPlan" TEXT,
     "creditsCost" INTEGER NOT NULL DEFAULT 0,
